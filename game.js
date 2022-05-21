3e9ecc95b90a984c980ed9391b999d0add1fe7fb
El Faisal Alaaeddine <faisalalaaeddine2000@gmail.com>
2022-05-21 21:23:39 +0300

    creating a function that touches border

--- game.js
+++ game.js
@@ -9,6 +9,14 @@ var game = document.getElementById('game')
 var hover = 0;
 var move = 0
 
+function crossedBorder(){
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.backgroundColor = "#ff8888"
+    }
+    score -= 10
+    stat.innerHTML = "Oops, You lost :( , Click S to start again, Current score "+ score
+}
+
 
 })
 
