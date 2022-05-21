4056b0e48fc2c7b903ca7108e9321b92500c01d8
El Faisal Alaaeddine <faisalalaaeddine2000@gmail.com>
2022-05-21 21:22:19 +0300

    declaring variables

---
+++ game.js
@@ -0,0 +1,14 @@
+window.addEventListener ("load", function(){
+var boolean = false;
+var score = 0;
+var stat = document.getElementById('status')
+var initial = document.getElementById("start")
+var ending = document.getElementById('end')
+var elements = document.getElementsByClassName('boundary');
+var game = document.getElementById('game')
+var hover = 0;
+var move = 0
+
+
+})
+
