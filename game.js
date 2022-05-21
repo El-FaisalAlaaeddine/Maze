a006bd93a008a03eaef6c8d1d7bd41c1187bab9b
El Faisal Alaaeddine <faisalalaaeddine2000@gmail.com>
2022-05-21 21:53:54 +0300

    removing event listener from boundaries after winning

--- game.js
+++ game.js
@@ -35,6 +35,9 @@ function crossedBorder(){
             hover+=1
             if (hover === 1){
                 ending.removeEventListener("mouseover", Winning)
+                for(var i = 0; i < elements.length; i++){
+                    elements[i].removeEventListener('mouseover',crossedBorder)
+                }
             }
             
         }}
